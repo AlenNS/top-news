9e5b89616ab650786744cfaf86a2821c70043e3c
Alen Cobic <alenns@gmail.com>
2020-11-21 02:22:13 +0100

    Add views and common components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,20 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { TranslateModule, TranslateService } from '@ngx-translate/core';
 
 import { SharedModule } from './shared/shared.module';
+import { TopNewsComponent } from './views/top-news/top-news.component';
+import { CategoryListComponent } from './views/category-list/category-list.component';
+import { SearchListComponent } from './views/search-list/search-list.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopNewsComponent,
+    CategoryListComponent,
+    SearchListComponent,
+    NavbarComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
