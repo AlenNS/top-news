c8d57b80a9c129b5ed0cbf5b132c4ce348bcd624
Alen Cobic <alen.cobic@ebs.msft>
2020-11-21 01:51:30 +0100

    Set basic project structure and add bootstrap

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'top-news'`, () => {
+  it(`should have as title 'Top News App'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('top-news');
+    expect(app.title).toEqual('Top News App');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('top-news app is running!');
+    expect(compiled.querySelector('.container span').textContent).toContain('Top News App is running!');
   });
 });
