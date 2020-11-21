2d00b3f49c23cc2bca219aa9a6e8c148cebe90aa
Alen Cobic <alenns@gmail.com>
2020-11-21 02:16:10 +0100

    Add angular material dependencies

---
+++ src/app/shared/ng-material-dependencies.module.ts
@@ -0,0 +1,146 @@
+import {
+  NgModule
+} from '@angular/core';
+import {
+  CommonModule
+} from '@angular/common';
+
+import {
+  MatSnackBarModule
+} from '@angular/material/snack-bar';
+import {
+  MatAutocompleteModule
+} from '@angular/material/autocomplete';
+import {
+  MatButtonModule
+} from '@angular/material/button';
+import {
+  MatButtonToggleModule
+} from '@angular/material/button-toggle';
+import {
+  MatTabsModule
+} from '@angular/material/tabs';
+import {
+  MatSelectModule
+} from '@angular/material/select';
+import {
+  MatInputModule
+} from '@angular/material/input';
+import {
+  MatFormFieldModule
+} from '@angular/material/form-field';
+import {
+  MatTooltipModule
+} from '@angular/material/tooltip';
+import {
+  MatCheckboxModule
+} from '@angular/material/checkbox';
+import {
+  MatRadioModule
+} from '@angular/material/radio';
+import {
+  MatIconModule
+} from '@angular/material/icon';
+import {
+  MatChipsModule
+} from '@angular/material/chips';
+import {
+  MatSlideToggleModule
+} from '@angular/material/slide-toggle';
+import {
+  MatMenuModule
+} from '@angular/material/menu';
+import {
+  MatSliderModule
+} from '@angular/material/slider';
+import {
+  MatTableModule
+} from '@angular/material/table';
+import {
+  MatSortModule
+} from '@angular/material/sort';
+import {
+  MatPaginatorModule
+} from '@angular/material/paginator';
+import {
+  MatDatepickerModule
+} from '@angular/material/datepicker';
+import {
+  MatExpansionModule
+} from '@angular/material/expansion';
+import {
+  MatProgressBarModule
+} from '@angular/material/progress-bar';
+import {
+  MatDividerModule
+} from '@angular/material/divider';
+import {
+  MatDialogModule
+} from '@angular/material/dialog';
+import {
+  MatNativeDateModule
+} from '@angular/material/core';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    MatSnackBarModule,
+    MatDialogModule,
+    MatAutocompleteModule,
+    MatButtonModule,
+    MatButtonToggleModule,
+    MatTabsModule,
+    MatSelectModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatTooltipModule,
+    MatCheckboxModule,
+    MatRadioModule,
+    MatIconModule,
+    MatSnackBarModule,
+    MatChipsModule,
+    MatSlideToggleModule,
+    MatMenuModule,
+    MatSliderModule,
+    MatTableModule,
+    MatSortModule,
+    MatPaginatorModule,
+    MatDatepickerModule,
+    MatExpansionModule,
+    MatSortModule,
+    MatProgressBarModule,
+    MatDividerModule,
+    MatNativeDateModule,
+  ],
+  exports: [
+    MatSnackBarModule,
+    MatDialogModule,
+    MatAutocompleteModule,
+    MatButtonModule,
+    MatButtonToggleModule,
+    MatTabsModule,
+    MatSelectModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatTooltipModule,
+    MatCheckboxModule,
+    MatRadioModule,
+    MatIconModule,
+    MatSnackBarModule,
+    MatChipsModule,
+    MatSlideToggleModule,
+    MatMenuModule,
+    MatSliderModule,
+    MatTableModule,
+    MatSortModule,
+    MatPaginatorModule,
+    MatDatepickerModule,
+    MatExpansionModule,
+    MatSortModule,
+    MatProgressBarModule,
+    MatDividerModule,
+    MatNativeDateModule
+  ],
+  declarations: []
+})
+export class NgMaterialDependenciesModule {}
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,11 +3,14 @@ import { CommonModule } from '@angular/common';
 
 import { TranslateModule, TranslateService } from '@ngx-translate/core';
 
+import { NgMaterialDependenciesModule } from './ng-material-dependencies.module';
+
 @NgModule({
     declarations: [],
     imports: [
         CommonModule,
-        TranslateModule
+        TranslateModule,
+        NgMaterialDependenciesModule
     ],
     exports: [],
     providers: []
