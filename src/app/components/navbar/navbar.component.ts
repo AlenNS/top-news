c40469338402f9105c19d79c9f4032beaab92004
Alen Cobic <alenns@gmail.com>
2020-11-24 17:52:34 +0100

    Set active state for nav items

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,15 +1,60 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, DoCheck } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.scss']
 })
-export class NavbarComponent implements OnInit {
+export class NavbarComponent implements DoCheck {
+  isHome = false;
+  isCategories = false;
+  isSearch = false;
+
   showMenu = false;
 
-  constructor() { }
+  constructor(
+    private route: ActivatedRoute
+  ) {}
 
-  ngOnInit(): void {
+  ngDoCheck(): void {
+    this.getParams();
+  }
+
+  getParams(): void {
+    let active = location.pathname;
+    const singleCategory = '/category';
+
+    if (active.substring(0, 9) === singleCategory) {
+      active = singleCategory;
+    }
+
+    switch (active) {
+      case '/':
+        this.initActiveState();
+        this.isHome = true;
+        break;
+      case '/categories':
+      case '/category':
+        this.initActiveState();
+        this.isCategories = true;
+        break;
+      case '/search':
+        this.initActiveState();
+        this.isSearch = true;
+        break;
+
+      default:
+      case '/':
+        this.initActiveState();
+        this.isHome = true;
+        break;
+    }
+  }
+
+  initActiveState(): void {
+    this.isHome = false;
+    this.isCategories = false;
+    this.isSearch = false;
   }
 }
--- src/app/shared/services/top-news.service.ts
+++ src/app/shared/services/top-news.service.ts
@@ -7,14 +7,15 @@ import { catchError } from 'rxjs/operators';
 import { Response } from './../data-types/response';
 
 // Add proxy URL
-// const PROXY = 'https://cors-anywhere.herokuapp.com/';
-// API endpoint URL and api key values
-const BASE_URL = 'https://newsapi.org/v2/top-headlines?';
+const PROXY = 'https://cors-anywhere.herokuapp.com/';
 const API_KEY = '85c2278d358f439e899cd26d34b7b871';
 // const API_KEY = '64b10bfb4a8d4230b644895c0700b8d2'; // new API when first one limit is reached
 // const API_KEY = '0ca816426404456bbe37a705f6590e70'; // new API when first one limit is reached
 // const API_KEY = '2afcebc275a34077af2c446c8b326767'; // new API when first one limit is reached
 // const API_KEY = '3ba7ca94946444e7b61b4a199985f662'; // new API when first one limit is reached
+
+// API endpoint URL and api key values
+const BASE_URL = `${PROXY}https://newsapi.org/v2/top-headlines?apiKey=${API_KEY}`;
 
 const headers = new HttpHeaders({'Content-Type': 'application/json'});
 
@@ -25,7 +26,7 @@ export class TopNewsService {
     ) { }
 
     getNewsByCountry(country: string): Observable<Response> {
-        const FULL_URL = BASE_URL + 'apiKey=' + API_KEY + '&country=' + country;
+        const FULL_URL = BASE_URL + '&country=' + country;
 
         return this.http
           .get<Response>(FULL_URL, { headers })
@@ -33,7 +34,7 @@ export class TopNewsService {
     }
 
     getNewsByCategory(country: string, category: string, pageSize?: number): Observable<Response> {
-      let FULL_URL = BASE_URL + 'apiKey=' + API_KEY + '&country=' + country + '&category=' + category;
+      let FULL_URL = BASE_URL + '&country=' + country + '&category=' + category;
 
       if (pageSize) {
         FULL_URL += '&pageSize=' + pageSize;
@@ -45,7 +46,7 @@ export class TopNewsService {
     }
 
     getNewsByTerm(country: string, searchTerm: string): Observable<Response> {
-      const FULL_URL = BASE_URL + 'apiKey=' + API_KEY + '&country=' + country + '&q=' + searchTerm.toString();
+      const FULL_URL = BASE_URL + '&country=' + country + '&q=' + searchTerm.toString();
 
       return this.http
         .get<Response>(FULL_URL, { headers })
