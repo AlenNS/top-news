81fb7025a66bb61a6dc309ac3ee8c65ddfe62262
Alen Cobic <alenns@gmail.com>
2020-11-24 15:51:25 +0100

    Set new API key

--- src/app/shared/services/top-news.service.ts
+++ src/app/shared/services/top-news.service.ts
@@ -7,14 +7,14 @@ import { catchError } from 'rxjs/operators';
 import { Response } from './../data-types/response';
 
 // Add proxy URL
-const PROXY = 'https://cors-anywhere.herokuapp.com/';
+// const PROXY = 'https://cors-anywhere.herokuapp.com/';
 // API endpoint URL and api key values
 const BASE_URL = 'https://newsapi.org/v2/top-headlines?';
-// const API_KEY = '85c2278d358f439e899cd26d34b7b871';
+const API_KEY = '85c2278d358f439e899cd26d34b7b871';
 // const API_KEY = '64b10bfb4a8d4230b644895c0700b8d2'; // new API when first one limit is reached
 // const API_KEY = '0ca816426404456bbe37a705f6590e70'; // new API when first one limit is reached
 // const API_KEY = '2afcebc275a34077af2c446c8b326767'; // new API when first one limit is reached
-const API_KEY = '3ba7ca94946444e7b61b4a199985f662'; // new API when first one limit is reached
+// const API_KEY = '3ba7ca94946444e7b61b4a199985f662'; // new API when first one limit is reached
 
 const headers = new HttpHeaders({'Content-Type': 'application/json'});
 
@@ -25,7 +25,7 @@ export class TopNewsService {
     ) { }
 
     getNewsByCountry(country: string): Observable<Response> {
-        const FULL_URL = PROXY + BASE_URL + 'apiKey=' + API_KEY + '&country=' + country;
+        const FULL_URL = BASE_URL + 'apiKey=' + API_KEY + '&country=' + country;
 
         return this.http
           .get<Response>(FULL_URL, { headers })
@@ -33,7 +33,7 @@ export class TopNewsService {
     }
 
     getNewsByCategory(country: string, category: string, pageSize?: number): Observable<Response> {
-      let FULL_URL = PROXY + BASE_URL + 'apiKey=' + API_KEY + '&country=' + country + '&category=' + category;
+      let FULL_URL = BASE_URL + 'apiKey=' + API_KEY + '&country=' + country + '&category=' + category;
 
       if (pageSize) {
         FULL_URL += '&pageSize=' + pageSize;
@@ -45,7 +45,7 @@ export class TopNewsService {
     }
 
     getNewsByTerm(country: string, searchTerm: string): Observable<Response> {
-      const FULL_URL = PROXY + BASE_URL + 'apiKey=' + API_KEY + '&country=' + country + '&q=' + searchTerm.toString();
+      const FULL_URL = BASE_URL + 'apiKey=' + API_KEY + '&country=' + country + '&q=' + searchTerm.toString();
 
       return this.http
         .get<Response>(FULL_URL, { headers })
