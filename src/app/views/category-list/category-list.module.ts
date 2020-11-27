b1889381715c25712d5647a96175e4111d73d0a7
Alen Cobic <alenns@gmail.com>
2020-11-27 12:22:55 +0100

    Clean the code

--- src/app/views/category-list/category-list.module.ts
+++ src/app/views/category-list/category-list.module.ts
@@ -1,10 +1,9 @@
-import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
+import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Routes, RouterModule} from '@angular/router';
 
 import { SharedModule } from './../../shared/shared.module';
 import { CategoryListComponent } from './category-list.component';
-import { MatAccordion, MatExpansionModule } from '@angular/material/expansion';
 
 const routes: Routes = [
   {
